9732b8f034a546ac9bfde5d7cd5e8a497aa74946
AngelicaTrumbower <a_m_hello@yahoo.com>
2024-11-11 15:30:29 -0500

    hotcollections bug

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+
 
 const HotCollections = () => {
+
+  const [collectionsData, setcollectionsData] = useState([])
+  const [itemsArray, setItemsArray] = useState([])
+  useEffect(() => {
+    async function hotCollections() {
+      const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`)
+      console.log(data)
+      setcollectionsData(data)
+      setItemsArray(data[2].items)
+      console.log(itemsArray)
+    } 
+    hotCollections();
+  }, []);
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,23 +27,23 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {itemsArray.map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <img src={itemsArray.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={itemsArray.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{itemsArray.title}</h4>
                   </Link>
                   <span>ERC-192</span>
                 </div>
@@ -42,5 +55,4 @@ const HotCollections = () => {
     </section>
   );
 };
-
 export default HotCollections;
